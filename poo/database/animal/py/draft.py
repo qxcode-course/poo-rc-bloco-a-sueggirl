9558f404e9e591a8e604dd0771a5a30be09a5ead
sueggirl <heysuegirl@gmail.com>
2025-10-05 19:30:24 -0300

    summit

--- poo/database/animal/py/draft.py
+++ poo/database/animal/py/draft.py
@@ -1,6 +1,58 @@
-class animal:
-    def __init__(self, sound: str, age: int):
+class Animal:
+    def __init__(self, species: str, sound: str):
+        self.species: str = species
         self.sound: str = sound
         self.age: int = 0
+
+    def makeSound(self) -> str:
+        if self.age == 0:
+            return "---"
+        elif self.age >= 4:
+            return "RIP"        
+        else:
+            return self.sound
+
+    def ageBy(self, increment: int) -> None:
+        if self.age >= 4:
+            print(f"warning: {self.species} morreu")
+            return
         
-    def main():
+        self.age += increment
+        if self.age >= 4:
+            self.age = 4
+            print(f"warning: {self.species} morreu")
+            
+    def __str__(self) -> str:
+        return f"{self.species}:{self.age}:{self.sound}"
+    
+        
+        
+def main():
+    animal: Animal = Animal("", "")
+    while True:
+        line: str = input()
+        print("$" + line)
+        args: list[str] = line.split(" ")
+
+        if args[0] == "end":
+            break
+
+        elif args[0] == "init":
+            species: str = args[1]
+            sound: str = args[2]
+            animal = Animal(species, sound)
+
+        elif args[0] == "show":
+            print(animal)
+
+        elif args[0] == "grow":
+            increment: int = int(args[1])
+            animal.ageBy(increment)
+
+        elif args[0] == "noise":
+            print(animal.makeSound())
+
+        else:
+            print("fail: comando não encontrado")
+
+main()
